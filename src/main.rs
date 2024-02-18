d6c4e8b72db9cebe4729ecf7885b3ee3330c6fb6
Cade Warg <wargcade@gmail.com>
2024-02-17 18:31:45 -0600

    added map generation and player

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
 mod map;
+mod player;
+mod map_builder;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::player::*;
+    pub use crate::map_builder::*;
 }
 
 use prelude::*;
 
 struct State {
     map: Map,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
         Self {
-            map: Map::new()
+            map: map_builder.map,
+            player :Player::new(map_builder.player_start),
         }
     }
 }
@@ -24,7 +32,9 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
